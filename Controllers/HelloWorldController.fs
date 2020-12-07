55c16c69ebdb63bf1bbbbd7a8dfee8c9ec582a97
Ed Cavazos <wayo.cavazos@gmail.com>
2020-12-07 02:14:13 -0800

    HelloWorldController Index

--- Controllers/HelloWorldController.fs
+++ Controllers/HelloWorldController.fs
@@ -1,2 +1,9 @@
 namespace MvcMovieFs.Controllers
 
+open Microsoft.AspNetCore.Mvc
+open System.Text.Encodings.Web
+
+type HelloWorldController () =
+    inherit Controller()
+
+    member this.Index () = "default action"
